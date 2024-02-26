1eaa37f1d13c0b6d7b103c35d903be0f022a93a8
Ed Byrne <ed.byrne@cna.nl.ca>
2024-02-26 08:33:20 -0330

    Unit 5 Closures Example

---
+++ Unit 5/slide_show_closure/lib_slide_show.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const createSlideShow = () => {
+    // private variables and functions
+
+    // we need a timer, a play state, a speed and an object of nodes and an object of images
+    // the nodes must include images and captions
+    // the image object must include a cache array and a counter
+
+    // we must also have a functin to stop the slideshow
+    // and we need a function to display the next image
+    
+    
+    // public methods that have access to private variables and functions
+    
+    // we need to be able to loadImages by passing a list of slides
+
+    // we need to be able to start a slide show, which will use a timer to display the next side based on the speed
+
+    // we need a way of toggling the play pause
+
+};
