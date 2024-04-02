03d9355a9410bd038e5a19a9de8044f2a43a1b41
Ed Byrne <ed.byrne@cna.nl.ca>
2024-04-02 09:14:08 -0230

    nasa with lib example

---
+++ Unit 6/astronomy_pod_modify/apod.js
@@ -0,0 +1,34 @@
+"use strict";
+
+
+// display error
+const displayError = error => {
+    let html = `<span class="error">${error.message}</span>`;
+    $("#display").html(html);
+}
+
+// deal with clicks on the view button
+
+$(document).ready( () => { 
+
+    // deal with the button click
+    $("#view_button").click( () =>{
+        // get date from textbox
+        let dateStr = $("#date").val();
+        const dateObj = new Date(dateStr);
+
+        // make sure date  is valid
+        if (dateObj == "Invalid Date"){
+            const msg = "Please enter valid date in YYYY-MM-DD foramt.";
+            displayError(new Error(msg));
+            // $("#display").html(`<span class="error">${msg}</span`);
+        }else{
+
+            getPictureData( dateObj );
+
+        }
+        $("#date").focus();
+
+       
+    })
+});
